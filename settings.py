dc5ea75a2dd24647d3015b0ea3b05445dc2c7fd1
kaspeya <89647596+kaspeya@users.noreply.github.com>
2023-05-01 18:52:20 +0300

    Add consts

--- settings.py
+++ settings.py
@@ -8,3 +8,17 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Всесто MY SECRET KEY придумайте и впишите свой ключ
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+MAX_LENGTH = 6
+MAX_LEN_CUSTOM_ID = 16
+
+HOST = 'localhost'
+
+NO_REQUEST_BODY = 'Отсутствует тело запроса'
+NO_REQUIRED_FIELD = '\"url\" является обязательным полем!'
+INVALID_URL_NAME = 'Указано недопустимое имя для короткой ссылки'
+NOT_FOUND = 'Указанный id не найден'
+
+FORM_LEN_MSG = 'Короткая ссылка должна быть не более 16 символов'
+FORM_REQUIRED_FIELD = 'Обязательное поле'
