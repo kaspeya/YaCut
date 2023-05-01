741013b0af8d7cb0f6235a5eb37583b03ec828fc
kaspeya <89647596+kaspeya@users.noreply.github.com>
2023-05-01 19:03:42 +0300

    add default='sqlite:///db.sqlite3'

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     # Подключается БД SQLite
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     # Задаётся конкретное значение для конфигурационного ключа
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Всесто MY SECRET KEY придумайте и впишите свой ключ
